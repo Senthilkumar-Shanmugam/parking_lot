5a64acdd32d0e400e5fdaeca3e46fa5af1d96141
sss.senthil@gmail.com <sss.senthil@gmail.com>
2018-09-24 21:33:01 +0530

    eclipse project
    intial eclipse project

---
+++ GOJEK/src/main/java/com/assigment/GOJEK/App.java
@@ -0,0 +1,13 @@
+package com.assigment.GOJEK;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
