3589f1d0c7386278134d7ced2e21d6ca27e6d082
sss.senthil@gmail.com <sss.senthil@gmail.com>
2018-09-24 21:47:56 +0530

    parkingLot testcase

---
+++ GOJEK/src/test/java/com/assigment/GOJEK/ParkingLotTest.java
@@ -0,0 +1,11 @@
+package com.assigment.GOJEK;
+
+import junit.framework.TestCase;
+
+public class ParkingLotTest extends TestCase {
+	
+	public void testParkingLotExists() {
+		ParkingLot parkLot = new ParkingLotImpl();
+	}
+
+}
