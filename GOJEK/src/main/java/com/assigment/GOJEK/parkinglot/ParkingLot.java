1cbd75e32f525b7182a2d0a4c7a8377f5622e0ff
sss.senthil@gmail.com <sss.senthil@gmail.com>
2018-09-26 20:33:43 +0530

    added protected access to internal data structures

--- GOJEK/src/main/java/com/assigment/GOJEK/parkinglot/ParkingLot.java
+++ GOJEK/src/main/java/com/assigment/GOJEK/parkinglot/ParkingLot.java
@@ -25,6 +25,22 @@ public abstract class ParkingLot implements ParkingLotI {
 		for(int slot=1;slot<=number;slot++)
 			slots.add(new Slot(slot,0));// since we are not handling any usecases related to level, assing all slots to level 0
 	}
+
+	protected PriorityQueue<Slot> getSlots() {
+		return slots;
+	}
+
+	protected Map<String, Slot> getRegNumSlotMap() {
+		return regNumSlotMap;
+	}
+
+	protected Map<Slot, Car> getSlotCarMap() {
+		return slotCarMap;
+	}
+
+	protected Map<Color, List<Car>> getColorCarMap() {
+		return colorCarMap;
+	}
 		
 
 }
