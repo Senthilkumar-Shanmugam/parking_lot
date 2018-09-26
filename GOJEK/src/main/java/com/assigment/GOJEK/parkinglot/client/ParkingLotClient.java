9a3ce10ba8943ce98fc87a2441edd69e8f76edba
sss.senthil@gmail.com <sss.senthil@gmail.com>
2018-09-26 09:49:20 +0530

    refactor

--- GOJEK/src/main/java/com/assigment/GOJEK/parkinglot/client/ParkingLotClient.java
+++ GOJEK/src/main/java/com/assigment/GOJEK/parkinglot/client/ParkingLotClient.java
@@ -3,9 +3,7 @@ package com.assigment.GOJEK.parkinglot.client;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
 
 import com.assigment.GOJEK.parkinglot.Car;
