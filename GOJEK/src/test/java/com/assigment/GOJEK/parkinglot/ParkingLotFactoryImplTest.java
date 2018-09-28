99925dfecf284c88bc141ee8a471dae54ae49f70
sss.senthil@gmail.com <sss.senthil@gmail.com>
2018-09-28 21:07:11 +0530

    test class added for parkinglotFactoryImpl

---
+++ GOJEK/src/test/java/com/assigment/GOJEK/parkinglot/ParkingLotFactoryImplTest.java
@@ -0,0 +1,13 @@
+package com.assigment.GOJEK.parkinglot;
+
+import junit.framework.TestCase;
+
+public class ParkingLotFactoryImplTest extends TestCase {
+
+	protected void setUp() throws Exception {
+	}
+
+	protected void tearDown() throws Exception {
+	}
+
+}
