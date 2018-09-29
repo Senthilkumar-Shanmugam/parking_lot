f542d7b36cccad8552a115f7276775cbff2a347a
sss.senthil@gmail.com <sss.senthil@gmail.com>
2018-09-29 11:28:51 +0530

    test class added for parkingLot Client

---
+++ GOJEK/src/test/java/com/assigment/GOJEK/parkinglot/client/ParkingLotClientTest.java
@@ -0,0 +1,17 @@
+package com.assigment.GOJEK.parkinglot.client;
+
+import junit.framework.TestCase;
+
+public class ParkingLotClientTest extends TestCase {
+
+	protected void setUp() throws Exception {
+	}
+
+	protected void tearDown() throws Exception {
+	}
+	
+	public void testParkingLotClientExists() {
+		assertTrue(true);
+	}
+
+}
