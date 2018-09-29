01b0f86037636c6b7d7fba30b0180da54eb251fc
sss.senthil@gmail.com <sss.senthil@gmail.com>
2018-09-29 10:55:48 +0530

    test added for parkingLot factory impl

--- GOJEK/src/test/java/com/assigment/GOJEK/parkinglot/ParkingLotFactoryImplTest.java
+++ GOJEK/src/test/java/com/assigment/GOJEK/parkinglot/ParkingLotFactoryImplTest.java
@@ -1,17 +1,23 @@
 package com.assigment.GOJEK.parkinglot;
 
+import com.assigment.GOJEK.parklot.exception.InsufficentNumberOfSlots;
+import com.assigment.GOJEK.parklot.exception.NoSuchParkingLotException;
+
 import junit.framework.TestCase;
 
 public class ParkingLotFactoryImplTest extends TestCase {
+	ParkingLotFactory parkFactory;
+	ParkingLotType parkingLotType;
 
 	protected void setUp() throws Exception {
+		parkFactory = new ParkingLotFactoryImpl();
 	}
 
 	protected void tearDown() throws Exception {
 	}
 	
-	public void testParkingLotExists() {
-		assertTrue(true);
+	public void testParkingLotFactoryReturnsPakingLotIfThatParkingLotTypeExists() throws NoSuchParkingLotException, InsufficentNumberOfSlots {
+		assertNotNull(parkFactory.getParkingLot(ParkingLotType.PUBLIC, 1));
 	}
 
 }
